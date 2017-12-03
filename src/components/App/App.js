e0ecfd1982b2eca1f8f74d4730a2679db950c219
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-03 17:49:54 -0200

    Fix app style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.sass';
+import style from './App.sass';
 
-const App = () => <div className="app">Hello world!</div>;
+const App = () => <div className={style.app}>Hello world!</div>;
 
 export default App;
