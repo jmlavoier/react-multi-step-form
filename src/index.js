e9662f81343841115761b4953a2a4bc7e0962756
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-03 18:40:19 -0200

    Removed serviceWorks

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.sass';
 import App from './components/App';
-import registerServiceWorker from './utils/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
