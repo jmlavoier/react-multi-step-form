f5304dbb5ff9fca84390e67028d1e11c7f2f5dd8
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-03 21:46:37 -0200

    Created the Form to be more DRY

--- src/forms/FirstStepForm/FirstStepForm.js
+++ src/forms/FirstStepForm/FirstStepForm.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { createFormStepsStore } from 'utils';
 
 import Container from 'components/Container';
 import Item from 'components/Item';
 import InputCheckBox from 'components/InputCheckBox';
-import Box from 'components/Box';
-import Header from 'components/Header';
-import StepsWrapper from 'components/StepsWrapper';
-
-
-const HeaderForm = (
-  <Header>
-    <Container justifyContent="center">
-      <Item>
-        <StepsWrapper steps={4} currentStep={0} />
-      </Item>
-    </Container>
-  </Header>
-);
+import Form from 'forms/Form';
 
 const FirstStepForm = () => (
-  <Box header={HeaderForm}>
-    <Container alignItems="flex-start" justifyContent="center">
-      <Item flexGrow="1" styles={{ textAlign: 'center', paddingBottom: '20px' }}>
-        <p>Wellcome to the Multi Step Form, lets start!</p>
-      </Item>
-    </Container>
+  <Form text="Wellcome to the Multi Step Form, lets start!">
     <Container alignItems="flex-start" justifyContent="center">
       <Item styles={{ textAlign: 'center', width: '100px' }}>
         <InputCheckBox name="a1" label="A1" />
@@ -34,7 +15,7 @@ const FirstStepForm = () => (
         <InputCheckBox name="a2" label="A2" />
       </Item>
     </Container>
-  </Box>
+  </Form>
 );
 
 export default FirstStepForm;
