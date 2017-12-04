24eac74a0d3dd77c82e8633247484f655605b7b4
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-03 23:08:28 -0200

    Refactored App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+
+import Container from 'components/Container';
+import Item from 'components/Item';
+
+import ComposeAllForms from 'forms/ComposeAllForms';
+
 import style from './App.sass';
 
-const App = () => <div className={style.app}>Hello world!</div>;
+const App = () => (
+  <div className={style.app}>
+    <Container justifyContent="center" height="100px">
+      <Item flexGrow="1" styles={{ textAlign: 'center', paddingTop: '20px', color: '#fff' }}>
+        <h1>Multi Step Form</h1>
+      </Item>
+    </Container>
+    <Container justifyContent="center" height="100%">
+      <ComposeAllForms step={0} />
+    </Container>
+  </div>
+);
 
 export default App;
