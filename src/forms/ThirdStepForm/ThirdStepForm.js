29d934b437764d7bd01bf77066c803e7870b2451
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-03 22:08:13 -0200

    Added ThirdStepForm

---
+++ src/forms/ThirdStepForm/ThirdStepForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Container from 'components/Container';
+import Item from 'components/Item';
+import InputText from 'components/InputText';
+import Button from 'components/Button';
+import Form from 'forms/Form';
+
+const ThirdStepForm = () => (
+  <Form text="Wellcome to the Multi Step Form, lets start!">
+    <Container alignItems="flex-start" justifyContent="center">
+      <Item styles={{ width: '200px' }}>
+        <InputText name="email" label="E-mail" />
+      </Item>
+      <Item styles={{ textAlign: 'center', padding: '18px 10px' }}>
+        <Button text="Check Your E-mail" />
+      </Item>
+    </Container>
+  </Form>
+);
+
+export default ThirdStepForm;
