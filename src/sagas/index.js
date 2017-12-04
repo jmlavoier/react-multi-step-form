c8b55e967929abc5a457f1b48edae78b48af54c5
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-04 19:13:23 -0200

    Added Fourth Step Saga to combine

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,11 +3,13 @@ import { all } from 'redux-saga/effects';
 import firstStepSaga from 'containers/FirstStepForm/sagas';
 import secondStepSaga from 'containers/SecondStepForm/sagas';
 import thirdStepSaga from 'containers/ThirdStepForm/sagas';
+import fourthStepSaga from 'containers/FourthStepForm/sagas';
 
 export default function* rootSaga() {
   yield all([
     ...firstStepSaga,
     ...secondStepSaga,
     ...thirdStepSaga,
+    ...fourthStepSaga,
   ]);
 }
