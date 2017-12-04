6989094c4cb65448bcf3e4f0a6dd1fe40862834d
Joao Lavoier <jmlavoier@hotmail.com>
2017-12-04 19:48:53 -0200

    Fix name field text

--- src/containers/ThirdStepForm/ThirdStepForm.js
+++ src/containers/ThirdStepForm/ThirdStepForm.js
@@ -11,10 +11,10 @@ const ThirdStepForm = ({ onChangeInput, thirdStepForm, onClickCheck }) => (
   <Form text={thirdStepForm.errorMessage} currentStep={2}>
     <Container alignItems="flex-start" justifyContent="center">
       <Item styles={{ width: '200px' }}>
-        <InputText name="email" label="E-mail" value={thirdStepForm.text} onChange={e => onChangeInput(e.target.value)} />
+        <InputText name="email" label="Name" value={thirdStepForm.text} onChange={e => onChangeInput(e.target.value)} />
       </Item>
       <Item styles={{ textAlign: 'center', padding: '18px 10px' }}>
-        <Button text="Check Your E-mail" onClick={() => onClickCheck(thirdStepForm.text)} />
+        <Button text="Check Your Name" onClick={() => onClickCheck(thirdStepForm.text)} />
       </Item>
     </Container>
   </Form>
